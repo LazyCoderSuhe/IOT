537cd650b515f4856e563611165e649a918adca1
suhe0919@live.com <suhe0919@live.com>
2021-06-21 22:02:44 +0800

    添加项目文件。

---
+++ HThread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.System.Threading;
+
+namespace SH.IOTLIb
+{
+    public class HThread
+    {
+        WorkItemHandler workItem;
+        public HThread(WorkItemHandler handler)
+        {
+            if (handler == null)
+            {
+                handler = call;
+            }
+            workItem = handler;
+        }
+        public async Task Start()
+        {
+            await Windows.System.Threading.ThreadPool.RunAsync(workItem, WorkItemPriority.High);
+        }
+
+        private void call(IAsyncAction operation)
+        {
+            Console.WriteLine("为实现方法,这也即是 IOT 实现的  线程");
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace SH.IOTLIb
+{
+    /// <summary>
+    /// 这里只是 DispatcherTimer  示例代码   可用
+    /// </summary>
+    public class Timer
+    {
+        private DispatcherTimer timer;
+        public Timer(EventHandler<object> Timer_Tick, TimeSpan span)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = span;
+            timer.Tick += Timer_Tick;
+            timer.Start();           
+        }
+
+        public void Stop()
+        {
+            timer.Stop();            
+        }
+      
+    }
+}
